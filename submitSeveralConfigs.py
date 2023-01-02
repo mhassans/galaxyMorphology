9741a3bd74a60e210a18f145b5ee9c7696458618
MohammadHassan <mhhassanshahi@yahoo.com>
2023-01-02 23:58:41 +0000

    generalize batch submission script

--- submitSeveralConfigs.py
+++ submitSeveralConfigs.py
@@ -2,13 +2,21 @@ import os
 import sys
 from funcs import produceConfig, setConfigName
 
+def run(config, submitToBatch):
+    fileName = setConfigName(config)
+    filePath = produceConfig(config, fileName)
+    if (submitToBatch):
+        os.system('qsub -v input="' + filePath + '" glxMorph.sh')
+    else:
+        os.system('python main.py '+ filePath + '> log/' + fileName + '.out')
+
 def main(submitToBatch):
     #initialize config
     config = dict(
         load_model = False,
         class_weight = None,
         classical = True,
-        gamma = 'scale',
+        gamma = 'auto',
         C_class = 1.0e+6,
         alpha = 0.1,
         C_quant = 1.0e+6,
@@ -22,21 +30,47 @@ def main(submitToBatch):
         modelSavedPath = 'trainedModels/',
         resultOutputPath = 'output/'
     )
+   
+    list_classical = [True, False]
+    list_class_weight = [None, 'balanced']
+    list_fold_idx = list(range(config['n_splits']))
     
-    #for clfType in [True, False]:
-    #    config['classical'] = clfType
-    #for Cclass in [0.1, 1.0, 10, 100]:
-    for Cclass in [1000]:
-    #for Cclass in [1.0e+4, 1.0e+5, 1.0e+6, 1.0e+7, 1.0e+8]:
-        config['C_class'] = Cclass
-        for i in range(config['n_splits']):
-            config['fold_idx'] = i
-            fileName = setConfigName(config)
-            filePath = produceConfig(config, fileName)
-            if (submitToBatch):
-                os.system('qsub -v input="' + filePath + '" glxMorph.sh')
-            else:
-                os.system('python main.py '+ filePath + '> log/' + fileName + '.out')
+    #Classic-only
+    list_C_class = [0.1, 1.0, 10, 100, 1000, 1.0e+4, 1.0e+5, 1.0e+6, 1.0e+7, 1.0e+8]
+    list_gamma = [1, 'scale', 'auto']
+    
+    #Quantum-only
+    list_alpha = [0.1]
+    list_C_quant = [1.0e+6]
+    list_single_mapping = [1]
+    list_pair_mapping = [1]
+    list_interaction = ['YY']
+    
+    for clfType in list_classical:
+        config['classical'] = clfType
+        for weight in list_class_weight:
+            config['class_weight'] = weight
+            for foldID in list_fold_idx:
+                config['fold_idx'] = foldID
+                
+                if config['classical']:
+                    for Cclass in list_C_class:
+                        config['C_class'] = Cclass
+                        for gamma in list_gamma:
+                            config['gamma'] = gamma
+                            run(config, submitToBatch) #run
+                else:
+                    for alpha in list_alpha:
+                        config['alpha'] = alpha
+                        for CQuant in list_C_quant:
+                            config['C_quant'] = CQuant
+                            for snglMap in list_single_mapping:
+                                config['single_mapping'] = snglMap
+                                for pairMap in list_pair_mapping:
+                                    config['pair_mapping'] = pairMap
+                                    for interaction in list_interaction:
+                                        config['interaction'] = interaction
+                                        run(config, submitToBatch) #run
 
 if __name__ == "__main__":
     submitToBatch = False
